8678fafd4397d940932436be65411418e0e71a6c
chriswebb09 <chris.webb5249@gmail.com>
2016-05-24 23:44:10 -0400

    Code edit

---
+++ To-Do-List-Extension-ios/ChecklistItem.swift
@@ -0,0 +1,15 @@
+//
+//  ChecklistItem.swift
+//  To-Do-List-Extension-ios
+//
+//  Created by Christopher Webb-Orenstein on 5/24/16.
+//  Copyright © 2016 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import Foundation
+
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
